5a01325664cdb92158b0f86e48b286f22f51c14f
Maitrehenry Nathanaël <nathamhy@gmail.com>
2024-10-11 15:35:53 +0200

    add test

--- app/projet/src/main/java/module-info.java
+++ app/projet/src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module projet.projet {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires junit;
 
     opens projet.projet to javafx.fxml;
+    exports test to junit;
     exports projet.projet;
 }
---
+++ app/projet/src/main/java/test/SyllogismTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SyllogismTest {
+    @Test
+    public void test1(){
+        assertTrue(true);
+    }
+}
