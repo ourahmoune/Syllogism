27a62e64cefc835802906b8431bed9910cde2d44
diewertje <diewertjevandam@gmail.com>
2024-10-14 16:21:20 +0200

    Creation of Rp rule and tests

---
+++ app/projet/src/main/java/test/RpTest.java
@@ -0,0 +1,57 @@
+package test;
+
+import app.Model.*;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class RpTest {
+    @Test
+    public void testTrue(){
+        //Création des quantificateurs
+        Quantificator q1 = new Quantificator(Quantity.Exisential, "Il y a");
+        Quantificator q2 = new Quantificator(Quantity.Universal, "Certains");
+
+        // Création des propositions
+        Proposition prop1 = new Proposition(q1, "homme", "mortel", Quality.Negative, Type.A);
+        Proposition prop2 = new Proposition(q2, "philosophes", "homme", Quality.Negative, Type.A);
+        Proposition prop3 = new Proposition(q1, "philosophes", "mortels", Quality.Affirmative, Type.A);
+
+        // Ajout des propositions dans une map
+        Map<Integer, Proposition> propositions = new HashMap<>();
+        propositions.put(1, prop1);
+        propositions.put(2, prop2);
+        propositions.put(3, prop3);
+
+        Syllogism syllogism = new Syllogism(Figure.UN, propositions);
+        Rp rule= new Rp();
+        assertTrue(rule.Launch(syllogism));
+    }
+    @Test
+    public void testFalse(){
+        //Création des quantificateurs
+        Quantificator q1 = new Quantificator(Quantity.Universal, "Tous");
+        Quantificator q2 = new Quantificator(Quantity.Exisential, "Certains");
+        Quantificator q3 = new Quantificator(Quantity.Universal, "Certains");
+
+        // Création des propositions
+        Proposition prop1 = new Proposition(q1, "hommes", "mortels", Quality.Affirmative, Type.A);
+        Proposition prop2 = new Proposition(q2, "philosophes", "hommes", Quality.Affirmative, Type.A);
+        Proposition prop3 = new Proposition(q3, "philosophes", "mortels", Quality.Negative, Type.A);
+
+        // Ajout des propositions dans une map
+        Map<Integer, Proposition> propositions = new HashMap<>();
+        propositions.put(1, prop1);
+        propositions.put(2, prop2);
+        propositions.put(3, prop3);
+
+        Syllogism syllogism = new Syllogism(Figure.UN, propositions);
+        Rp rule= new Rp();
+        assertFalse(rule.Launch(syllogism));
+    }
+
+}
