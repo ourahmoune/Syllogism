ef80796d02edcdec799f61735f6bd33548701f0b
ourahmoune <nadir.ourahmoune.dz@gmail.com>
2024-10-21 14:14:49 +0200

    Queleque modifcation dans mes fichiers

---
+++ app/projet/src/main/java/app/Model/AllRules/SyllogismAndRules.java
@@ -0,0 +1,27 @@
+package app.Model.AllRules;
+
+import app.Model.Syllogism;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SyllogismAndRules {
+    private Syllogism syllogism;
+    List<Boolean> Rules = new ArrayList<>();
+    public SyllogismAndRules() {
+        syllogism= null ;
+    }
+    public void setSyllogism(Syllogism syllogism) {
+        this.syllogism = syllogism;
+    }
+    public  Syllogism getSyllogism() {
+        return syllogism;
+    }
+    public void setRules(boolean Rule) {
+        Rules.add(Rule);
+    }
+    public boolean getRules(int index) {
+        return Rules.get(index);
+    }
+
+}
