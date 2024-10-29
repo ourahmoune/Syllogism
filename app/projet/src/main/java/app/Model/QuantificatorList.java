a0cb02af572ee4abb9b385ee51aff684c88c9357
Clement chioni <150047450+Crawler06@users.noreply.github.com>
2024-10-29 10:51:11 +0100

    la partie List of quantifiers est normalement fini

--- app/projet/src/main/java/app/Model/QuantificatorList.java
+++ app/projet/src/main/java/app/Model/QuantificatorList.java
@@ -1,27 +1,49 @@
 package app.Model;
 
-import java.util.Map;
+import java.util.*;
 
 public class QuantificatorList {
 
-	private Map<Quantificator, Quantity> quantificators;
+	// Instance unique de la classe (singleton)
+	private static QuantificatorList instance;
 
-	/**
-	 * 
-	 * @param quantificator
-	 */
-	public void addQuantificator(Quantificator quantificator) {
-		// TODO - implement QuantificatorList.addQuantificator
-		throw new UnsupportedOperationException();
+	// La liste des quantificateurs (utilise HashMap pour la gestion des éléments)
+	private List<Quantificator> quantificators;
+
+	// Constructeur privé pour empêcher la création d'instances
+	private QuantificatorList() {
+		this.quantificators = new ArrayList<Quantificator>();
 	}
 
-	/**
-	 * 
-	 * @param quantificator
-	 */
-	public void removeQuantificator(Quantificator quantificator) {
-		// TODO - implement QuantificatorList.removeQuantificator
-		throw new UnsupportedOperationException();
+	// Méthode pour obtenir l'instance unique (singleton)
+	public static QuantificatorList getInstance() {
+		if (instance == null) {
+			instance = new QuantificatorList();
+		}
+		return instance;
+	}
+
+	// Méthode pour ajouter un quantificateur à la liste
+	public void addQuantificator(Quantificator quantificator) {
+		quantificators.add(quantificator);
+	}
+
+	// Méthode pour retirer un quantificateur de la liste
+	public void removeQuantificator(String word, Quantity quantity) {
+		for (Quantificator quantificator : quantificators) {
+			if (quantificator.getQuantity() == quantity) {
+				if (quantificator.getName().equals(word)) {
+					System.out.println("Affichage : " + quantificator.getName() + " " + quantificator.getQuantity());
+					quantificators.remove(quantificator);
+					break;
+				}
+			}
+		}
+	}
+
+	// Accesseur pour obtenir la liste des quantificateurs
+	public List<Quantificator> getQuantificators() {
+		return quantificators;
 	}
 
 }
