cdcfd7905377e8168d63a43f3f975c4522b7d50e
DiewDiew <150831186+DiewDiew@users.noreply.github.com>
2024-11-05 11:00:30 +0100

    Settings toggel buttons: changed the id's and made shure at start all rules are true

--- app/projet/src/main/java/app/controller/SettingsController.java
+++ app/projet/src/main/java/app/controller/SettingsController.java
@@ -7,23 +7,23 @@ import javafx.scene.shape.Circle;
 
 public class SettingsController {
     @FXML
-    private StackPane knobContainer,knobContainer11,knobContainer111, knobContainer1111,knobContainer1, knobContainer11111,knobContainer111111,knobContainer1111111,knobContainer2;
+    private StackPane knobContainer,knobContainer1,knobContainer2,knobContainer3,knobContainer4,knobContainer5,knobContainer6,knobContainer7,knobContainer8;
     @FXML
-    private Label on,on1, on11, on111,on1111,on11111,on111111,on1111111,on2;
+    private Label on,on1, on2, on3,on4, on5,on6,on7,on8;
     @FXML
-    private Label off, off1, off11, off111, off1111, off11111, off111111, off1111111, off2;
+    private Label off, off1, off2, off3, off4, off5, off6, off7, off8;
     @FXML
     private Circle toggleBall,toggleBall1,toggleBall2,toggleBall3,toggleBall4,toggleBall5,toggleBall6,toggleBall7,toggleBall8;
 
-    private boolean isOn = false;
-    private boolean isOn1 = false;
-    private boolean isOn11 = false;
-    private boolean isOn111 = false;
-    private boolean isOn1111 = false;
-    private boolean isOn11111 = false;
-    private boolean isOn111111 = false;
-    private boolean isOn1111111 = false;
-    private boolean isOn2=false;
+    private boolean isOn = true;
+    private boolean isOn1 = true;
+    private boolean isOn2 = true;
+    private boolean isOn3 = true;
+    private boolean isOn4 = true;
+    private boolean isOn5 = true;
+    private boolean isOn6 = true;
+    private boolean isOn7 = true;
+    private boolean isOn8 = true;
 
 
 
@@ -35,56 +35,56 @@ public class SettingsController {
     }
     @FXML
     private void toggleSwitch2() {
-        isOn2 = !isOn2;
-        updateToggle(toggleBall1, on1, off1, isOn2);
+        isOn1 = !isOn1;
+        updateToggle(toggleBall1, on1, off1, isOn1);
         System.out.println("Toggle Switch 2 is " + (isOn1 ? "ON" : "OFF"));
     }
     @FXML
     private void toggleSwitch3() {
-        isOn11 = !isOn11;
-        updateToggle(toggleBall2, on11, off11, isOn11);
-        System.out.println("Toggle Switch 3 is " + (isOn11 ? "ON" : "OFF"));
+        isOn2 = !isOn2;
+        updateToggle(toggleBall2, on2, off2, isOn2);
+        System.out.println("Toggle Switch 3 is " + (isOn2 ? "ON" : "OFF"));
     }
 
     @FXML
     private void toggleSwitch4() {
-        isOn111 = !isOn111;
-        updateToggle(toggleBall3, on111, off111, isOn111);
-        System.out.println("Toggle Switch 4 is " + (isOn111 ? "ON" : "OFF"));
+        isOn3 = !isOn3;
+        updateToggle(toggleBall3, on3, off3, isOn3);
+        System.out.println("Toggle Switch 4 is " + (isOn3 ? "ON" : "OFF"));
     }
     @FXML
     private void toggleSwitch5() {
-        isOn1111 = !isOn1111;
-        updateToggle(toggleBall4, on1111, off1111, isOn1111);
-        System.out.println("Toggle Switch 5 is " + (isOn1111 ? "ON" : "OFF"));
+        isOn4 = !isOn4;
+        updateToggle(toggleBall4, on4, off4, isOn4);
+        System.out.println("Toggle Switch 5 is " + (isOn4 ? "ON" : "OFF"));
     }
 
     @FXML
     private void toggleSwitch6() {
-        isOn11111 = !isOn11111;
-        updateToggle(toggleBall5, on11111, off11111, isOn11111);
-        System.out.println("Toggle Switch 6 is " + (isOn11111 ? "ON" : "OFF"));
+        isOn5 = !isOn5;
+        updateToggle(toggleBall5, on5, off5, isOn5);
+        System.out.println("Toggle Switch 6 is " + (isOn5 ? "ON" : "OFF"));
     }
 
     @FXML
     private void toggleSwitch7() {
-        isOn111111 = !isOn111111;
-        updateToggle(toggleBall6, on111111, off111111, isOn111111);
-        System.out.println("Toggle Switch 7 is " + (isOn111111 ? "ON" : "OFF"));
+        isOn6 = !isOn6;
+        updateToggle(toggleBall6, on6, off6, isOn6);
+        System.out.println("Toggle Switch 7 is " + (isOn6 ? "ON" : "OFF"));
     }
 
     @FXML
     private void toggleSwitch8() {
-        isOn1111111 = !isOn1111111;
-        updateToggle(toggleBall7, on1111111, off1111111, isOn1111111);
-        System.out.println("Toggle Switch 8 is " + (isOn1111111 ? "ON" : "OFF"));
+        isOn7 = !isOn7;
+        updateToggle(toggleBall7, on7, off7, isOn7);
+        System.out.println("Toggle Switch 8 is " + (isOn7 ? "ON" : "OFF"));
     }
 
     @FXML
     private void toggleSwitch9() {
-        isOn2 = !isOn2;
-        updateToggle(toggleBall8, on2, off2, isOn2);
-        System.out.println("Toggle Switch 9 is " + (isOn2 ? "ON" : "OFF"));
+        isOn8 = !isOn8;
+        updateToggle(toggleBall8, on8, off8, isOn8);
+        System.out.println("Toggle Switch 9 is " + (isOn8 ? "ON" : "OFF"));
     }
 
     private void updateToggle(Circle knob, Label labelOn, Label labelOff, boolean isOn) {
@@ -99,7 +99,7 @@ public class SettingsController {
         }
     }
     private void setupToggleSwitch(StackPane knob,Circle circle, Label labelOn, Label labelOff, Runnable toggleAction) {
-        updateToggle(circle, labelOn, labelOff, false);
+        updateToggle(circle, labelOn, labelOff, true);
         knob.setOnMouseClicked(event -> toggleAction.run());
     }
 
@@ -107,13 +107,13 @@ public class SettingsController {
     public void initialize() {
         setupToggleSwitch(knobContainer,toggleBall, on, off, this::toggleSwitch1);
         setupToggleSwitch(knobContainer1,toggleBall1, on1, off1, this::toggleSwitch2);
-        setupToggleSwitch(knobContainer11,toggleBall2, on11, off11, this::toggleSwitch3);
-        setupToggleSwitch(knobContainer111,toggleBall3, on111, off111, this::toggleSwitch4);
-        setupToggleSwitch(knobContainer1111,toggleBall4, on1111, off1111, this::toggleSwitch5);
-        setupToggleSwitch(knobContainer11111,toggleBall5, on11111, off11111, this::toggleSwitch6);
-        setupToggleSwitch(knobContainer111111,toggleBall6, on111111, off111111, this::toggleSwitch7);
-        setupToggleSwitch(knobContainer1111111,toggleBall7, on1111111, off1111111, this::toggleSwitch8);
-        setupToggleSwitch(knobContainer2,toggleBall8, on2, off2, this::toggleSwitch9);
+        setupToggleSwitch(knobContainer2,toggleBall2, on2, off2, this::toggleSwitch3);
+        setupToggleSwitch(knobContainer3,toggleBall3, on3, off3, this::toggleSwitch4);
+        setupToggleSwitch(knobContainer4,toggleBall4, on4, off4, this::toggleSwitch5);
+        setupToggleSwitch(knobContainer5,toggleBall5, on5, off5, this::toggleSwitch6);
+        setupToggleSwitch(knobContainer6,toggleBall6, on6, off6, this::toggleSwitch7);
+        setupToggleSwitch(knobContainer7,toggleBall7, on7, off7, this::toggleSwitch8);
+        setupToggleSwitch(knobContainer8,toggleBall8, on8, off8, this::toggleSwitch9);
     }
 
 }
